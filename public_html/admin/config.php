08e402eea1fd9a584ac66e55ac3205eab7f4656a
huybqps03559 <huybqps03559@fpt.edu.vn>
2016-08-16 20:10:26 +0700

    huybqps0559

---
+++ public_html/admin/config.php
@@ -0,0 +1,31 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://huybqps03559.esy.es/admin/');
+define('HTTP_CATALOG', 'http://huybqps03559.esy.es/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://huybqps03559.esy.es/admin/');
+define('HTTPS_CATALOG', 'http://huybqps03559.esy.es/');
+
+// DIR
+define('DIR_APPLICATION', '/home/u460725616/public_html/admin/');
+define('DIR_SYSTEM', '/home/u460725616/public_html/system/');
+define('DIR_IMAGE', '/home/u460725616/public_html/image/');
+define('DIR_LANGUAGE', '/home/u460725616/public_html/admin/language/');
+define('DIR_TEMPLATE', '/home/u460725616/public_html/admin/view/template/');
+define('DIR_CONFIG', '/home/u460725616/public_html/system/config/');
+define('DIR_CACHE', '/home/u460725616/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/u460725616/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/u460725616/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/u460725616/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/u460725616/public_html/system/storage/upload/');
+define('DIR_CATALOG', '/home/u460725616/public_html/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.hostinger.vn');
+define('DB_USERNAME', 'u460725616_huy');
+define('DB_PASSWORD', 'loveforever');
+define('DB_DATABASE', 'u460725616_asm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
---
+++ public_html/config.php
@@ -0,0 +1,28 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://huybqps03559.esy.es/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://huybqps03559.esy.es/');
+
+// DIR
+define('DIR_APPLICATION', '/home/u460725616/public_html/catalog/');
+define('DIR_SYSTEM', '/home/u460725616/public_html/system/');
+define('DIR_IMAGE', '/home/u460725616/public_html/image/');
+define('DIR_LANGUAGE', '/home/u460725616/public_html/catalog/language/');
+define('DIR_TEMPLATE', '/home/u460725616/public_html/catalog/view/theme/');
+define('DIR_CONFIG', '/home/u460725616/public_html/system/config/');
+define('DIR_CACHE', '/home/u460725616/public_html/system/storage/cache/');
+define('DIR_DOWNLOAD', '/home/u460725616/public_html/system/storage/download/');
+define('DIR_LOGS', '/home/u460725616/public_html/system/storage/logs/');
+define('DIR_MODIFICATION', '/home/u460725616/public_html/system/storage/modification/');
+define('DIR_UPLOAD', '/home/u460725616/public_html/system/storage/upload/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'mysql.hostinger.vn');
+define('DB_USERNAME', 'u460725616_huy');
+define('DB_PASSWORD', 'loveforever');
+define('DB_DATABASE', 'u460725616_asm');
+define('DB_PORT', '3306');
+define('DB_PREFIX', 'oc_');
